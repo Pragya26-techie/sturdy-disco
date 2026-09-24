8a0e443bc9c3a4f5caac3ffb650fc31e311f55ad
Pragya26-techie <pragyarajput2662@gmail.com>
2026-09-24 20:17:44 +0530

    added memory to agent using langgraph

--- apps/2_google_agent.py
+++ apps/2_google_agent.py
@@ -4,6 +4,7 @@ load_dotenv()
 from langchain_community.utilities import GoogleSerperAPIWrapper
 from langchain_groq import ChatGroq
 from langchain.agents import create_agent
+from langgraph.checkpoint.memory import InMemorySaver
 
 llm = ChatGroq(model="openai/gpt-oss-20b")
 search = GoogleSerperAPIWrapper()
@@ -11,13 +12,16 @@ search = GoogleSerperAPIWrapper()
 agent = create_agent(
     model=llm,
     tools=[search.run],
+    checkpointer = InMemorySaver(),
     system_prompt="You are a agent and can search for any question on google."
 )
 
 while True:
     query = input("User:")
-    if query.lower=="quit":
+    if query.strip().lower()=="quit":
         print("Good Bye")
         break
-    response = agent.invoke({"messages":[{"role":"user","content":query}]})
+    response = agent.invoke(
+              {"messages":[{"role":"user","content":query}]},
+              {"configurable":{"thread_id":"1"}})
     print("AI:",response["messages"][-1].content)
